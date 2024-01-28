ff1769e579e44972c998fc456e19ba91e175cee7
Rishabh <rishu.prasad@hotmail.com>
2024-01-27 20:55:19 -0600

    Completed baseline intro form

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Preferences from "./pages/Preferences"
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Preferences />
+    {/* <App /> */}
   </React.StrictMode>
 );
 
